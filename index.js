2059117b862dcc5fcf8d16a083ba19d3b0339d0b
Chris George <chrisgeorge.9@gmail.com>
2019-11-29 23:19:13 -0800

    completed js for knowcurrentgrade screen

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const state = {
   clickedToSecondScreen: false,
-  knowCurrentGrade: false
+  knowCurrentGrade: false,
 };
 
-const app = document.querySelector(".app");
+const app = document.querySelector('.app');
 
 const knowCurrentGradeHTML = `
     <span class="goBack"><a href="/">&laquo;</a></span>
@@ -19,6 +19,7 @@ const knowCurrentGradeHTML = `
                 step="0.1"
                 name="currentGrade"
                 id="currentGrade"
+                value="100"
               />
               <span class="input-after">%</span>
             </div>
@@ -26,6 +27,84 @@ const knowCurrentGradeHTML = `
         </div>
 
         <h1>FINAL EXAM</h1>
+        <div class="bottom-card">
+          <div class="input-label-group">
+            <label for="estimatedGrade">What you think you'll get</label>
+            <div class="input-group">
+              <input
+                type="number"
+                min="0"
+                step="0.1"
+                value="100"
+                name="estimatedGrade"
+                id="estimatedGrade"
+              />
+              <span class="input-after">%</span>
+            </div>
+          </div>
+          <div class="input-label-group">
+            <label for="finalWeight">What it's worth</label>
+            <div class="input-group">
+              <input
+                type="number"
+                min="0"
+                step="0.1"
+                value="20"
+                name="finalWeight"
+                id="finalWeight"
+              />
+              <span class="input-after">%</span>
+            </div>
+          </div>
+        </div>
+`;
+
+const dontKnowCurrentGradeHTML = `
+     <span class="goBack"><a href="/">&laquo;</a></span>
+        <div class="top-card">
+          <h1>Your Final Mark</h1>
+          <h2 class="finalPercent">100%</h2>
+          <div class="marks">
+            <div class="mark-row">
+              <h4>Mark 1</h4>
+              <div class="what-you-got input-label-group">
+                <label for="currentGrade">What you got</label>
+                <div class="input-group">
+                  <input
+                    type="number"
+                    min="0"
+                    step="0.1"
+                    name="whatYouGot"
+                    id="whatYouGot-1"
+                  />
+                  <span class="input-after">%</span>
+                </div>
+                <!-- input-group-->
+              </div>
+              <!-- input-label-group-->
+              <div class="what-you-got input-label-group">
+                <label for="currentGrade">What it's Worth</label>
+                <div class="input-group">
+                  <input
+                    type="number"
+                    min="0"
+                    step="0.1"
+                    name="whatYouGot"
+                    id="whatYouGot-1"
+                  />
+                  <span class="input-after">%</span>
+                </div>
+                <!-- input-group-->
+              </div>
+              <!-- input-label-group-->
+            </div>
+
+          </div>
+          <!-- marks-->
+        </div>
+        <!-- top-card-->
+
+        <h1>FINAL EXAMS</h1>
         <div class="bottom-card">
           <div class="input-label-group">
             <label for="estimatedGrade">What you think you'll get</label>
@@ -54,19 +133,57 @@ const knowCurrentGradeHTML = `
             </div>
           </div>
         </div>
+        <button class="addMark">+ Add Mark</button>
 `;
 
-const dontKnowCurrentGradeHTML = `
-    <h1>I'M AN IDIOT</h1>
+const additionalMarkRow = `
+  <!-- mark-row-->
+  <div class="mark-row">
+    <h4>Mark 1</h4>
+    <div class="what-you-got input-label-group">
+      <label for="currentGrade">What you got</label>
+      <div class="input-group">
+        <input
+          type="number"
+          min="0"
+          step="0.1"
+          name="whatYouGot"
+          id="whatYouGot-1"
+        />
+        <span class="input-after">%</span>
+      </div>
+      <!-- input-group-->
+    </div>
+    <!-- input-label-group-->
+    <div class="what-you-got input-label-group">
+      <label for="currentGrade">What it's Worth</label>
+      <div class="input-group">
+        <input
+          type="number"
+          min="0"
+          step="0.1"
+          name="whatYouGot"
+          id="whatYouGot-1"
+        />
+        <span class="input-after">%</span>
+      </div>
+      <!-- input-group-->
+    </div>
+    <!-- input-label-group-->
+  </div>
+  <!-- mark-row-->
 `;
 
 // FUNCTIONS
 
 function handleAppClick(e) {
-  if (e.target.dataset.know === "true") {
+  if (e.target.classList.contains('addMark')) {
+    addNewMarkRow();
+  }
+  if (e.target.dataset.know === 'true') {
     state.knowCurrentGrade = true;
     loadSecondScreen();
-  } else if (e.target.dataset.know === "false") {
+  } else if (e.target.dataset.know === 'false') {
     state.knowCurrentGrade = false;
     loadSecondScreen();
   }
@@ -75,15 +192,41 @@ function handleAppClick(e) {
 function loadSecondScreen() {
   if (state.knowCurrentGrade) {
     app.innerHTML = knowCurrentGradeHTML;
-    app.classList.add("secondScreen");
-    app.classList.add("knowCurrentGrade");
+    app.classList.add('secondScreen');
+    app.classList.add('knowCurrentGrade');
   } else {
     app.innerHTML = dontKnowCurrentGradeHTML;
-    app.classList.add("secondScreen");
-    app.classList.add("dontKnowCurrentGrade");
+    app.classList.add('secondScreen');
+    app.classList.add('dontKnowCurrentGrade');
+  }
+}
+
+function addNewMarkRow() {
+  const html = document.querySelector('.marks');
+  html.innerHTML = html.innerHTML += additionalMarkRow;
+}
+
+function calculateCurrentGrade() {
+  console.log('ran');
+  let grade;
+  if (app.classList.contains('knowCurrentGrade')) {
+    const currentGrade = parseFloat(
+      document.getElementById('currentGrade').value
+    );
+    if (isNaN(currentGrade)) currentGrade = 0;
+    const guess = parseFloat(document.getElementById('estimatedGrade').value);
+    if (isNaN(guess)) guess = 0;
+    const finalWeight =
+      parseFloat(document.getElementById('finalWeight').value) / 100;
+    if (isNaN(finalWeight)) finalWeight = 0;
+
+    grade = (currentGrade * (1 - finalWeight) + guess * finalWeight).toFixed(1);
+    document.querySelector('.finalPercent').innerHTML = `${grade}%`;
   }
 }
 
 // EVENTS
 
-app.addEventListener("click", handleAppClick);
+app.addEventListener('click', handleAppClick);
+app.addEventListener('keyup', calculateCurrentGrade);
+app.addEventListener('change', calculateCurrentGrade);
